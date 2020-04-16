62d18e2986af6a3bfe2c77ae6e18ceeba051d6a0
kaphie <bkafrika144@gmail.com>
2020-04-16 16:06:03 +0300

    my first commit

---
+++ contact.py
@@ -0,0 +1,48 @@
+import pyperclip
+
+
+class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    contact_list = []
+
+    def _init_(self,first_name,last_name,number,email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+    def save_contact(self):
+        Contact.contact_list.append(self)
+
+    def delete_contact(self):
+        Contact.contact_list.remove(self)
+
+    @classmethod
+    def find_by_number(cls,number):
+        
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
+
+    @classmethod
+    def contact_exist(cls,number):
+        
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                    return True
+
+        return False
+
+    @classmethod
+    def display_contacts(cls):
+        
+        return cls.contact_list
+
+    @classmethod
+    def copy_email(cls,number):
+        contact_found = Contact.find_by_number(number)
+        pyperclip.copy(contact_found.email)
